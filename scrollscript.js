0fd23b1991c79ade52fc22b24f7d7355806b1ffa
Duke <114975455+RaveZam@users.noreply.github.com>
2023-10-02 14:03:37 +0800

    yes

--- scrollscript.js
+++ scrollscript.js
@@ -1,16 +1,20 @@
 const header = document.getElementsByClassName('main-header')[0]
-const body = document.getElementsByClassName('body')
+const slide = document.getElementsByClassName('slide')[0]
+const slide2 = document.getElementsByClassName('slide2')[0]
 
+console.log(slide)
 function handleScroll(){
     const scrollY = window.scrollY;
 
     // Add a class to the header when scrolling down
     if (scrollY > 30) { // Adjust the threshold as needed
         header.classList.add('scrolled');
-        body.classList.add('scrolled')
+        slide.classList.add('slide-out')
+        slide2.classList.add('slide-out')
     } else {
         header.classList.remove('scrolled');
-        body.classList.remove('scrolled');
+        slide.classList.remove('slide-out') 
+        slide2.classList.remove('slide-out')  
     }
 }
 
