29060e88b5e8caa0c4e4e01d228ac2872b6ad2c3
Duke <114975455+RaveZam@users.noreply.github.com>
2023-10-08 20:02:50 +0800

    Experience Section but will change soon to tabs section

--- scrollscript.js
+++ scrollscript.js
@@ -6,12 +6,32 @@ const body = document.getElementsByClassName('body')[0]
 const profile = document.getElementsByClassName('profilepic')[0]
 const profiledesc = document.getElementsByClassName('mini-about-me-desc')[0]
 const vidslide1 = document.getElementsByClassName('vid-slide1')[0]
+const time = document.getElementsByClassName('time')[0]
+const expdesc = document.getElementsByClassName('experience-desc')[0]
+
+let isScrolling = false;
+
+window.addEventListener('scroll', () => {
+    if (!isScrolling) {
+        // Set a flag to indicate scrolling is in progress
+        isScrolling = true;
+
+        // Your animation code goes here
+
+        // Debounce the scroll event by delaying the reset of the flag
+        setTimeout(() => {
+            isScrolling = false;
+        }, 5000); // Adjust the delay (in milliseconds) as needed
+    }
+});
+
+
 function handleScroll(){
     const scrollY = window.scrollY;
     console.log(scrollY)
 
     // Add a class to the header when scrolling down
-    if (scrollY > 100) { // Adjust the threshold as needed
+    if (scrollY > 200) { // Adjust the threshold as needed
         header.classList.add('scrolled');
         slide.classList.add('slide-out')
         slide2.classList.add('slide-out')
@@ -29,15 +49,28 @@ function handleScroll(){
         body.classList.add('section2scroll')
         profile.classList.add('profilescrolled')
         profiledesc.classList.add('descscrolled')
+        body.classList.remove('dark')
+        header.classList.remove('darkheader')
     } else {
         body.classList.remove('section2scroll')
         
     }
+    if (scrollY > 1200) { // Adjust the threshold as needed
+        body.classList.add('dark')
+        header.classList.add('darkheader')
+        time.classList.add('scroll')
+        expdesc.classList.add('scroll')
+    } else {
+       
+        
+    }
 
-    if (scrollY > 900) { // Adjust the threshold as needed
-    
+    if (scrollY > 1800) { // Adjust the threshold as needed
+        body.classList.remove('dark')
+        header.classList.remove('darkheader')
         h2slide.classList.add('scrolled')
         body.classList.remove('section2scroll')
+       
         body.classList.add('scrolled')
         vidslide1.classList.add('highlightslide1')
        
