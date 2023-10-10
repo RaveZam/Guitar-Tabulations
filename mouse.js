3257bc97686b202423860b10d169c9da7c79dc30
Duke <114975455+RaveZam@users.noreply.github.com>
2023-10-10 15:28:31 +0800

    Added more parallax effect

--- mouse.js
+++ mouse.js
@@ -1,14 +1,18 @@
 document.addEventListener("mousemove", parallax)
 function parallax(e){
+  this.querySelectorAll('.layer').forEach(layer =>{
+       
     var slide = document.getElementsByClassName("welcome-slide")[0]
     var slide2 = document.getElementsByClassName("slide2")[0]
-    const speed = slide.getAttribute('data-speed')
+    const speed = layer.getAttribute('data-speed')
 
     const x = (window.innerWidth - e.pageX*speed)/120
     const y = (window.innerHeight - e.pageY*speed)/120
 
     slide.style.transform = `translateX(${x}px) translateY(${y}px)`
     slide2.style.transform = `translateX(${x}px) translateY(${y}px)`
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
 
-    
+    })
 }
+
